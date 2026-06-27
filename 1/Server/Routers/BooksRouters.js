47e159849cb45088ab1aab31a26f6f29403bc3af
hemant gavali <digitaluser197@gmail.com>
2026-06-27 12:00:00 +0530

    Done today

--- 1/Server/Routers/BooksRouters.js
+++ 1/Server/Routers/BooksRouters.js
@@ -4,6 +4,8 @@ const router = express.Router();
 const { PostBook, GetBook, PostUser, PostStaff, getStaff } = require("../Controller/BookPostController");
 
 router.post("/bookpost", PostBook);
+router.get("/bookget", GetBook);
+router.post("/register", PostUser);
 router.post("/staffpost", PostStaff);
 router.get("/viewstaff", getStaff)
 
