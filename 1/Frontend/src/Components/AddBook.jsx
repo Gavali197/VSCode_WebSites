512b56837c8152a98d645d1dd5b5e1a227532b98
hemant gavali <digitaluser197@gmail.com>
2026-06-30 11:36:06 +0530

    Save Work Today

--- 1/Frontend/src/Components/AddBook.jsx
+++ 1/Frontend/src/Components/AddBook.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
 
 const AddBook = () => {
@@ -9,6 +9,8 @@ const AddBook = () => {
     Price: "",
   });
 
+  const [get, setget] = useState([]);
+
   const [error, seterror] = useState("");
 
   const API = "http://localhost:4000/api/v2";
@@ -17,6 +19,22 @@ const AddBook = () => {
     const { name, value } = e.target;
     setform({ ...Form, [name]: value });
   };
+
+  const getBook = async () => {
+    try {
+      const res = await fetch(`${API}/bookget`);
+      const result = await res.json();
+      setget(result);
+    } catch (err) {
+      console.log(err);
+
+      seterror("Failed TO get data");
+    }
+  };
+
+  useEffect(() => {
+    getBook();
+  }, []);
 
   const handleForm = async (e) => {
     e.preventDefault();
@@ -39,6 +57,7 @@ const AddBook = () => {
         }),
       });
 
+      getBook();  
       const data = await res.json();
       if (!res.ok) {
         seterror(data.message);
@@ -48,20 +67,72 @@ const AddBook = () => {
       alert("Successfully");
       seterror("");
     } catch (err) {
-        console.log(err);
+      console.log(err);
       seterror("Server Side error");
     }
   };
-  return <>
-    <form onSubmit={handleForm}>
-        Title <input type="text" value={Form.Title} onChange={handleOnchange} name="Title" />
-        Description <input type="text" value={Form.Description} onChange={handleOnchange} name="Description" />
-        Author <input type="text" value={Form.Author} onChange={handleOnchange} name="Author" />
-        Price <input type="text" value={Form.Price} onChange={handleOnchange} name="Price" />
+  return (
+    <>
+      <div className="container-bookadd">
+        <form onSubmit={handleForm}>
+        Title{" "}
+        <input
+          type="text"
+          value={Form.Title}
+          onChange={handleOnchange}
+          name="Title"
+        />
+        Description{" "}
+        <input
+          type="text"
+          value={Form.Description}
+          onChange={handleOnchange}
+          name="Description"
+        />
+        Author{" "}
+        <input
+          type="text"
+          value={Form.Author}
+          onChange={handleOnchange}
+          name="Author"
+        />
+        Price{" "}
+        <input
+          type="text"
+          value={Form.Price}
+          onChange={handleOnchange}
+          name="Price"
+        />
         <button type="Submit">Add Book</button>
-        {error && <p style={{color:"red"}}>{error}</p>}
-    </form>
-  </>
+        {error && <p style={{ color: "red" }}>{error}</p>}
+      </form>
+      
+      </div>
+      <div className="detailed-container">
+        <table cellPadding={10} border={1}>
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Description</th>
+              <th>Author</th>
+              <th>Price</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {get.map((item, index)=>(
+              <tr key={index}>
+                <td>{item.Title}</td>
+                <td>{item.Description}</td>
+                <td>{item.Author}</td>
+                <td>{item.Price}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
 };
 
 export default AddBook;
