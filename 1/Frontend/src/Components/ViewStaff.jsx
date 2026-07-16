e3a9d1150e0a43e919453f71cf3d226446f70307
hemant gavali <digitaluser197@gmail.com>
2026-07-16 12:00:00 +0530

    Done today

--- 1/Frontend/src/Components/ViewStaff.jsx
+++ 1/Frontend/src/Components/ViewStaff.jsx
@@ -1,20 +1,6 @@
 import React, { useEffect } from 'react'
 import { useState } from 'react'
 
-const ViewStaff = () => {
-    const [select, setselect] = useState("");
-    const [staff, setstaff] = useState([])
-    const API = "http://localhost:4000/api/v2/staffpost"
-    const getStaff = async () =>{
-       try{
-         const get = await fetch(API)
-        const result = await res.json()
-        setstaff(result)
-       }catch(err){
-        console.log(err);
-        
-       }
-    }
 
     useEffect(()=>{
       getStaff();
