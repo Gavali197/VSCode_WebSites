9ad5ed4e483720736e98d6c0cbf8815cff2f3b2a
hemant gavali <digitaluser197@gmail.com>
2026-08-04 01:11:56 +0530

    Used Json web token

--- 1/Server/Controller/UserController.js
+++ 1/Server/Controller/UserController.js
@@ -1,26 +1,37 @@
 const user = require("../Models/UserModel");
+const bcrypt = require('bcrypt');
+const jwt = require("jsonwebtoken");
+
 
 exports.loginUser = async (req, res, next) => {
     try {
-      const {email, password} = req.body;
+        const { email, password } = req.body;
 
-      if(!email || !password){
-        return res.status(400).json({
-            success : false,
-            message : "Email password required"
-        })
-      }
-        const FindUser = await user.findOne({email});
+        if (!email || !password) {
+            return res.status(400).json({
+                success: false,
+                message: "Email password required"
+            })
+        }
+        const FindUser = await user.findOne({ email });
 
-        if(!FindUser){
+        if (!FindUser) {
             return res.status(401).json({
-                success : false,
-                message : "Invalid email or password"
+                success: false,
+                message: "Invalid email or password"
             })
         }
 
-        const isMatch = await compa
+        const isMatch = await bcrypt.compare(password, user.password);
 
-        
+        if (!isMatch) {
+            return res.status(401).json({
+                success: false,
+                message: "Invalid Email or password"
+            })
+        }
+
+        const token = 
+
     }
 }
